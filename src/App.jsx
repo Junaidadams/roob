179f3eaf5b98f7619297d79e829167b85787b306
Junaid Adams <junaidadams117@gmail.com>
2025-01-07 21:25:30 +0200

    End of day changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ function App() {
   return (
     <main className="-mt-[56px]">
       <section
-        className="min-h-screen bg-gradient-to-t to-pastelgreen to-80% from-pastelblue"
+        className="min-h-screen bg-pastelgreen flex flex-col-reverse md:flex-row"
         id=""
       >
         {" "}
-        <div></div>
+        <div className="md:w-1/3 w-3/4 bg-gradient-to-b from-pastelpink to-pastelblue border-black border rounded-full m-auto">
+          <img src="./heroimg.png" className="py-5" />
+        </div>
+        <div className="md:w-1/3 w-3/4 m-auto text-center">
+          <h1 className="md:text-6xl text-4xl font-bold">Hi, I&apos;m roob!</h1>
+          <p className="text-lg">Welcome to my website</p>
+        </div>
       </section>
-      <section className="min-h-screen bg-pastelblue" id="">
-        <div className="bg-radial-pastels to-pastelpurple from-pastelblue border border-black m-auto w-1/2 h-96 rounded px-4 shadow-xl">
-          <h1 className="text-7xl font-bold text-white bg-black py-5 px-3 rounded-b-2xl w-fit">
+      <section className="min-h-screen bg-pastelgreen flex" id="">
+        <div className="bg-[#f5f5f5] border border-black m-auto w-3/4 lg:w-3/5 h-96 rounded px-4 ">
+          <h1 className="lg:text-6xl text-xl sm:text-2xl md:text-4xl font-bold text-white bg-black py-4 px-4 rounded-b-2xl w-fit">
             Pricing
           </h1>
         </div>
       </section>
-      <section className="min-h-screen bg-pastelblue" id=""></section>
+      <section className="min-h-screen bg-pastelgreen" id=""></section>
     </main>
   );
 }
